d856e4d3a4f78c9b2bba8e66c0f955179b4d2be3
lyo-n <1y0mmmm@gmail.com>
2020-11-29 12:13:35 +0200

    Updates

--- precache-manifest.07734e63ac722859bf13794062448b1a.js
+++ precache-manifest.07734e63ac722859bf13794062448b1a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6e324d5b2b90a7bff8af12868ef14d05",
+    "revision": "3a6e18954e7dfa5bdc10655dc2fd25b6",
     "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "bd87b0a191cc48ee54a5",
+    "revision": "65e22d3c489695502f23",
     "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/css/main.f9d34802.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/2.1af95d3e.chunk.js"
   },
   {
-    "revision": "bd87b0a191cc48ee54a5",
-    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/main.32727d31.chunk.js"
+    "revision": "65e22d3c489695502f23",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/main.16b06eae.chunk.js"
   },
   {
     "revision": "d763282395b6873fe785",
