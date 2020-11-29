e1c51131ce0cfbb8d43043cc00abdbf19fe4a6a5
lyo-n <1y0mmmm@gmail.com>
2020-11-29 03:14:56 +0200

    Updates

---
+++ precache-manifest.07734e63ac722859bf13794062448b1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6e18954e7dfa5bdc10655dc2fd25b6",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "65e22d3c489695502f23",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/css/main.f9d34802.chunk.css"
+  },
+  {
+    "revision": "f0d6ad04a3463b13f9e4",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/2.1af95d3e.chunk.js"
+  },
+  {
+    "revision": "65e22d3c489695502f23",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/main.16b06eae.chunk.js"
+  },
+  {
+    "revision": "d763282395b6873fe785",
+    "url": "/github.io/goit-fe-cource_main/tree/master/goit-react-hw-01-components/static/js/runtime-main.eded2f51.js"
+  }
+]);
